66cf9e30988517c00783410db376725d0293899b
sophia banda <sophia.m.banda@gmail.com>
2019-10-05 20:33:52 -0400

    create entry module and component for easy extension

--- declarations.d.ts
+++ declarations.d.ts
@@ -1,2 +1,11 @@
 declare module '*';
  
+/*
+
+  A wildcard module is declared below to allow third party libraries to be used in an app even if they don't
+
+  provide their own type declarations.
+
+ */
+
+declare module '*';
